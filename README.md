65f6575e9c47eaab2109bade66e51f179733e53c
Alec Newman <alecnwmn904@gmail.com>
2018-02-11 13:36:38 -0700

    Works pretty well

--- README.md
+++ README.md
@@ -3,16 +3,54 @@ build
 
 Simple build tool to replace make.
 
-Goals/principles
-----------------
+Features:
 
-- Simple language for defining build dependencies.  In this case, YAML + shell
-  commands.
-- Atomic build steps.  Intermediate build products should be kept out of the
-  way.  If a build step fails midway through it won't dump the partial created
-  contents.
-- Built in watching/serving.  Easy to watch dependencies and automatically
-  rebuild and possibly restart a server if changes are made.
-- Efficient.  Parallel where possible.
-- Dependencies can be identified after a build step is done.  Can even be
-  automatically determined from kernel traces.
+- Artifact recipes are isolated and atomic.  Each recipe is run in an isolated
+  build directory, where it can only see dependencies it's explicitly included,
+  and can't modify any files outside the directory.  Instead each recipe
+  produces an artifact.  If the recipe fails, the artifact isn't created at all
+  rather than a partial artifact.
+- Automated watching and serving.  `build watch target` will build target, then
+  automatically rebuild it if any dependencies change.  There can also be serve
+  targets.  For instance `build watch serve-api` might serve an API over HTTP
+  locally; if any dependencies change the server will automatically be restart
+  with the new changes.
+- Automatically parallel.  Since recipes are isolated, they will be safely and
+  automatically run in parallel where possible.
+- YAML, so you don't have to learn a new syntax.
+
+The build configuration for a project is specified in a `build.yml` file.
+Here's an example of one:
+
+	node_modules:
+		- include node_modules package.json
+		- yarn
+		- mv node_modules $OUT
+
+	index.css:
+		- include node_modules index.scss
+		- node_modules/.bin/sass index.scss >$OUT
+	
+	bundle.js:
+		- include node_modules app.ts
+		- node_modules/.bin/tslint app.ts
+		- node_modules/.bin/tsc --strictNullChecks --noImplicitAny app.ts
+		- browserify app.ts >$OUT
+
+	dist:
+		- mkdir $OUT
+		- include index.html index.css bundle.js -into $OUT
+
+	serve-dist:
+		- include dist
+		- serve-http dist
+
+A recipe runs in an isolated directory.  When the special `include` command is
+used, its arguments will be built if necessary and then linked into the
+isolated directory.  The build step places its output into the special $OUT
+path, which can refer to either a file or directory.
+
+Build steps prefixed with `serve-` are special.  They are expected to not
+produce any outputs, but instead start a long-running process like an HTTP or
+API server.  If any of the dependencies change, the server will automatically
+be restarted with the new changes.
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
@@ -10,10 +11,12 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 )
 
 var config map[string][]string
+
 var originalDir = func() string {
 	result, err := os.Getwd()
 	if err != nil {
@@ -22,158 +25,166 @@ var originalDir = func() string {
 	return result
 }()
 
-func interpolate(word string, env map[string]string) string {
-	if strings.HasPrefix(word, "$") {
-		return env[strings.TrimPrefix(word, "$")]
-	} else {
-		return word
-	}
-}
-
-func hexify(text string) string {
+func tokenize(text string) string {
 	hash := sha256.Sum256([]byte(text))
 	return hex.EncodeToString(hash[:8])
 }
 
-func build(target string, args ...string) string {
-	if len(args) == 0 {
-		if err := os.Chdir(originalDir); err != nil {
-			panic(err)
+type includeCommand struct {
+	artifacts []string
+	into      string
+}
+
+type execCommand struct {
+	command string
+}
+
+func interpretCommand(command string) interface{} {
+	words := strings.Fields(command)
+
+	if words[0] == "include" {
+		cmd := includeCommand{
+			artifacts: nil,
+			into:      ".",
 		}
 
-		if _, err := os.Stat(target); err == nil {
-			path, err := filepath.Abs(target)
-			if err != nil {
+		for i := 1; i < len(words); i++ {
+			if words[i] == "-into" {
+				i++
+				cmd.into = words[i]
+			} else {
+				cmd.artifacts = append(cmd.artifacts, words[i])
+			}
+		}
+
+		return &cmd
+	} else {
+		return &execCommand{
+			command: command,
+		}
+	}
+}
+
+func build(target string) string {
+	log.Printf("build %q", target)
+
+	commands, recipeExists := config[target]
+
+	if !recipeExists {
+		// If the artifact is present in our source dir, we just include it.
+		if stat, err := os.Stat(filepath.Join(originalDir, target)); err == nil {
+			artifactKey := tokenize(fmt.Sprintf("%#v%#v", target, stat.ModTime().Unix()))
+			artifactPath := filepath.Join(originalDir, ".build", "artifact-"+artifactKey)
+
+			log.Printf("%s -> %s", target, artifactKey)
+
+			if err := os.Symlink(filepath.Join(originalDir, target), artifactPath); err != nil && !os.IsExist(err) {
 				panic(err)
 			}
 
-			return path
+			return artifactPath
 		} else if !os.IsNotExist(err) {
 			panic(err)
+		} else {
+			panic(fmt.Sprintf("Don't know how to build %q", target))
 		}
 	}
 
-	if _, ok := config[target]; !ok {
-		panic(fmt.Sprintf("Don't know how to build %q", target))
+	dependencies := make(map[string]string)
+	orderedDependencies := make([]string, 0)
+	keyMaterial := make([]string, 0)
+
+	for _, command := range commands {
+		keyMaterial = append(keyMaterial, command)
+		switch cmd := interpretCommand(command).(type) {
+		case *execCommand:
+		case *includeCommand:
+			for _, artifact := range cmd.artifacts {
+				dependencies[artifact] = build(artifact)
+				orderedDependencies = append(orderedDependencies, artifact)
+			}
+		default:
+			panic("bad command")
+		}
 	}
 
-	artifactKey := hexify(fmt.Sprintf("%#v%#v", target, args))
-	resultPath := filepath.Join(originalDir, ".build", "artifact-"+artifactKey)
+	sort.Strings(orderedDependencies)
 
-	if _, err := os.Stat(resultPath); err == nil {
-		return resultPath
+	for _, artifact := range orderedDependencies {
+		keyMaterial = append(keyMaterial, artifact, dependencies[artifact])
+	}
+
+	artifactKey := tokenize(strings.Join(keyMaterial, strings.Join(keyMaterial, "")))
+	artifactPath := filepath.Join(originalDir, ".build", "artifact-"+artifactKey)
+
+	if _, err := os.Stat(artifactPath); err == nil {
+		return artifactPath
 	} else if !os.IsNotExist(err) {
 		panic(err)
 	}
 
-	dir := filepath.Join(originalDir, ".build", "build-"+artifactKey)
-	//defer os.RemoveAll(dir)
+	outputPath := filepath.Join(originalDir, ".build", "artifact-output-"+artifactKey)
+	defer os.RemoveAll(outputPath)
 
-	if err := os.Mkdir(dir, 0700); err != nil {
+	buildDir := filepath.Join(originalDir, ".build", "build-"+artifactKey)
+	if err := os.Mkdir(buildDir, 0700); err != nil {
 		panic(err)
 	}
+	defer os.RemoveAll(buildDir)
 
-	env := make(map[string]string)
+	for _, commandString := range commands {
+		switch command := interpretCommand(commandString).(type) {
+		case *includeCommand:
+			command.into = os.Expand(command.into, func(name string) string {
+				if name == "OUT" {
+					return outputPath
+				} else {
+					panic("Unknown variable " + name)
+				}
+			})
 
-	for i, arg := range args {
-		env[fmt.Sprintf("%d", i+1)] = arg
-	}
+			for _, artifact := range command.artifacts {
+				artifactPath, ok := dependencies[artifact]
+				if !ok {
+					panic("artifact not built somehow")
+				}
 
-	log.Printf("build %s %v (dir=%s)", target, args, dir)
+				var intoPath string
+				if filepath.IsAbs(command.into) {
+					intoPath = filepath.Join(command.into, artifact)
+				} else {
+					intoPath = filepath.Join(buildDir, artifact)
+				}
 
-	for _, command := range config[target] {
-		words := strings.Fields(command)
-
-		switch words[0] {
-		case "exec":
-			for i, word := range words {
-				words[i] = interpolate(word, env)
-			}
-
-			var stdout = os.Stderr
-
-			if strings.HasPrefix(words[len(words)-1], ">") {
-				name := strings.TrimPrefix(words[len(words)-1], ">")
-				f, err := os.Create(name)
-				if err != nil {
+				if err := os.Symlink(artifactPath, intoPath); err != nil {
 					panic(err)
 				}
-				defer f.Close()
-
-				stdout = f
-				words = words[:len(words)-1]
 			}
-
-			log.Printf("exec %s %v >%s", words[1], words[2:], stdout.Name())
-			cmd := exec.Command(words[1], words[2:]...)
-			cmd.Dir = dir
-			cmd.Stdout = stdout
+		case *execCommand:
+			log.Printf("Executing %q", command.command)
+			cmd := exec.Command("sh", "-c", command.command)
+			cmd.Stdout = os.Stderr
 			cmd.Stderr = os.Stderr
-
+			cmd.Dir = buildDir
+			cmd.Env = append(cmd.Env, "OUT="+outputPath)
 			if err := cmd.Run(); err != nil {
 				panic(err)
 			}
-		case "produce":
-			if err := os.Chdir(dir); err != nil {
-				panic(err)
-			}
-
-			if err := os.Rename(words[1], resultPath); err != nil {
-				panic(err)
-			}
-		case "use":
-			var target string
-			var args []string
-			var dest string
-
-			if words[len(words)-2] == "-o" {
-				target = words[1]
-				args = words[2 : len(words)-2]
-				dest = words[len(words)-1]
-			} else {
-				target = words[1]
-				args = words[2:]
-				dest = target
-			}
-
-			actualArgs := make([]string, 0, len(args))
-
-			if err := os.Chdir(dir); err != nil {
-				panic(err)
-			}
-
-			for _, arg := range args {
-				path, err := filepath.Abs(arg)
-				if err != nil {
-					panic(err)
-				}
-
-				actualArgs = append(actualArgs, path)
-			}
-
-			log.Printf("use %s %v -o %s", target, actualArgs, dest)
-
-			result := build(target, actualArgs...)
-			if result == "" {
-				panic(fmt.Sprintf("%s produced no results", words[1]))
-			}
-
-			if err := os.Chdir(dir); err != nil {
-				panic(err)
-			}
-
-			if err := os.Symlink(result, dest); err != nil {
-				panic(err)
-			}
 		default:
-			panic(fmt.Sprintf("Unknown word %s", words[0]))
+			panic("bad command")
 		}
 	}
 
-	return resultPath
+	if err := os.Rename(outputPath, artifactPath); err != nil {
+		panic(err)
+	}
+
+	return artifactPath
 }
 
 func main() {
+	flag.Parse()
+
 	if err := os.MkdirAll(filepath.Join(originalDir, ".build"), 0700); err != nil {
 		panic(err)
 	}
@@ -187,5 +198,16 @@ func main() {
 		panic(err)
 	}
 
-	build("frontend")
+	for _, artifact := range flag.Args() {
+		artifactPath := build(artifact)
+		log.Printf("%s -> %s", artifact, artifactPath)
+
+		if err := os.Remove(artifact); err != nil && !os.IsNotExist(err) {
+			panic(err)
+		}
+
+		if err := os.Symlink(artifactPath, artifact); err != nil {
+			panic(err)
+		}
+	}
 }
